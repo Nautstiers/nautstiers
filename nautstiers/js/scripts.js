40de4ac4854eeb8b6ce86ef6ddd665acc8035192
Richard Bleek <richardbleek@gmail.com>
2015-01-21 18:49:27 +0100

    fixed merge issue
    fixed merge issue

--- nautstiers/js/scripts.js
+++ nautstiers/js/scripts.js
@@ -1,29 +1,6 @@
 var positions = {};
 
 $(function() {
-<<<<<<< HEAD
-    $("#draggable1").draggable();
-	$("#draggable2").draggable();
-	$("#draggable3").draggable();
-	$("#draggable4").draggable();
-	$("#draggable5").draggable();
-	$("#draggable6").draggable();
-	$("#draggable7").draggable();
-	$("#draggable8").draggable();
-	$("#draggable9").draggable();
-	$("#draggable10").draggable();
-	$("#draggable11").draggable();
-	$("#draggable12").draggable();
-	$("#draggable13").draggable();
-	$("#draggable14").draggable();
-	$("#draggable15").draggable();
-	$("#draggable16").draggable();
-	$("#draggable17").draggable();
-	$("#draggable18").draggable();
-	$("#draggable19").draggable();
-	$("#draggable20").draggable();
-	$("#draggable21").draggable();
-=======
     $(".drag-image").draggable({
     	drag: function(){
             var offset = $(this).offset();
@@ -35,7 +12,6 @@ $(function() {
             //positions.push({this.src()});
     	}
     });
->>>>>>> origin/master
 });
 
 img1=new Image();
