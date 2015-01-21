53633246a94cb4a16f1e7bd194ad1249bcc69239
Richard Bleek <richardbleek@gmail.com>
2015-01-21 16:38:45 +0100

    formatting
    formatting and css / script separation

---
+++ nautstiers/js/scripts.js
@@ -0,0 +1,64 @@
+$(function() {
+    $("#draggable1").draggable();
+	$("#draggable2").draggable();
+	$("#draggable3").draggable();
+	$("#draggable4").draggable();
+	$("#draggable5").draggable();
+	$("#draggable6").draggable();
+	$("#draggable7").draggable();
+	$("#draggable8").draggable();
+	$("#draggable9").draggable();
+	$("#draggable10").draggable();
+	$("#draggable11").draggable();
+	$("#draggable12").draggable();
+	$("#draggable13").draggable();
+	$("#draggable14").draggable();
+	$("#draggable15").draggable();
+	$("#draggable16").draggable();
+	$("#draggable17").draggable();
+	$("#draggable18").draggable();
+	$("#draggable19").draggable();
+	$("#draggable20").draggable();
+});
+
+img1=new Image();
+img1.src="afbeeldingen/Grid_Field.png";
+img2=new Image();
+img2.src="afbeeldingen/Grid_Ribbit.png";
+img3=new Image();
+img3.src="afbeeldingen/Grid_Aiguillon.png";
+img4=new Image();
+img4.src="afbeeldingen/Grid_Sorona.png";
+img5=new Image();
+img5.src="afbeeldingen/Grid_205.png";
+img6=new Image();
+img6.src="afbeeldingen/Grid_404.png";
+img7=new Image();
+img7.src="afbeeldingen/Grid_Random.png";
+
+function changeImage1() {
+    document.getElementById('myImage').src=img1.src;
+}
+
+function changeImage2() {
+    document.getElementById('myImage').src=img2.src;
+}
+function changeImage3() {
+document.getElementById('myImage').src=img3.src;
+}
+
+function changeImage4() {
+document.getElementById('myImage').src=img4.src;
+}
+
+function changeImage5() {
+document.getElementById('myImage').src=img5.src;
+}
+
+function changeImage6() {
+document.getElementById('myImage').src=img6.src;
+}
+
+function changeImage7() {
+document.getElementById('myImage').src=img7.src;
+}
